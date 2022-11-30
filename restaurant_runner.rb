3e7f5a1fc94a9825e55c9d652433d757ab5aa32e
Adam Bailey <ab67319@gmail.com>
2022-11-30 10:10:33 -0700

    Update restaurant_runner

--- restaurant_runner.rb
+++ restaurant_runner.rb
@@ -1,9 +1,33 @@
+#### Iteration 1 
+# require './lib/restaurant'
+
+# restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+# puts restaurant
+
+# puts restaurant.opening_time
+
+# puts restaurant.name
+# puts restaurant.dishes
+
+###############################
+#### Iteration 2
 require './lib/restaurant'
 
-restaurant = Restaurant.new('10:00', 'Fuel Cafe')
-puts restaurant
+restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+p restaurant1
 
-puts restaurant.opening_time
+restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+p restaurant2
+puts
 
-puts restaurant.name
-puts restaurant.dishes
+p restaurant1.closing_time(8)
+puts
+
+p restaurant2.closing_time(7)
+puts
+
+restaurant2.add_dish('Burrata')
+restaurant2.add_dish('Pizzetta')
+restaurant2.add_dish('Ravioli')
+
+p restaurant2.dishes
