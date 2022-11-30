a2f59e72da7e92147269627b5c2d57e87325fdc5
Jade Stewart <jadekstewart3@gmail.com>
2022-11-30 09:44:06 -0700

    Feat: add add_dish method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -13,7 +13,7 @@ class Restaurant
         "#{@opening_time.to_i + hours}:00"
     end
 
-    # def add_dish(dish)
-    #     @dishes << dish
-    # end
+    def add_dish(dish)
+        @dishes << dish
+    end
 end
