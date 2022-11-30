066ff7d3eb36411090bc1c3b64b6191327177fd5
Brian Hayes <Brian.hayes1200@gmail.com>
2022-11-30 09:56:30 -0700

    complete iteration 3

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -12,8 +12,19 @@ class Restaurant
     "#{closed}:00"
   end 
 
-  def add_dish(dish_name)
-    @dishes << dish_name
+  def add_dish(dish)
+    @dishes << dish
   end 
 
+  def open_for_lunch?
+    if @opening_time.to_i < 12
+      true
+    else 
+      false
+    end 
+  end 
+
+  def menu_dish_names
+   dish_names = @dishes.map{|dish| dish.upcase}
+  end 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -46,5 +46,15 @@ RSpec.describe Restaurant do
       expect(restaurant1.open_for_lunch?).to eq(true)
       expect(restaurant2.open_for_lunch?).to eq(false)
     end 
+
+    it 'will list menu names' do 
+      restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+      restaurant2 = Restaurant.new('16:00', 'Il Posto')
+      restaurant2.add_dish('Burrata')
+      restaurant2.add_dish('Pizzetta')
+      restaurant2.add_dish('Ravioli')
+
+      expect(restaurant2.menu_dish_names).to eq(["BURRATA", "PIZZETTA", "RAVIOLI"])
+    end 
   end 
 end
