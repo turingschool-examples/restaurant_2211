4103f4f471f06217f058ec9c854c4826c1b2d128
Dawson Timmons <DawsonTimmons@gmail.com>
2022-11-30 09:33:09 -0700

    Complete test two of iteration one

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant
-    attr_reader :name, :opening_time
+    attr_reader :name, :opening_time, :dishes
   
     def initialize(opening_time, name)
         @opening_time = opening_time
         @name = name
+        @dishes = []
     end
 end
