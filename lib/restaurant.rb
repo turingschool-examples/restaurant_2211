f5175faf2ea03f396333f5dc742f7a155bf6d887
Adam Bailey <ab67319@gmail.com>
2022-11-30 09:40:15 -0700

    Create restaurant initialize method, instance variables and allow access through attr_reader

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant
-  
+    attr_reader :dishes,
+                :name,
+                :opening_time
+                
+  def initialize(opening_time, name)
+    @dishes = []
+    @name = name
+    @opening_time = opening_time
+  end
 end
