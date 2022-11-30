a24d88db61604c219fe0d1370683d70f7da890df
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-11-30 09:43:58 -0700

    Fixed typo 'restaurant_spec.rb', lines 23 -32

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,9 +1,18 @@
 class Restaurant
 
-    attr_reader :opening_time, :name, :dishes
+    attr_reader :opening_time, 
+                :name, 
+                :dishes, 
+                :closing_time
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
+    
   end
+
+  def closing_time(hours)
+    (@opening_time.to_i) + (hours) + ':00'
+  end
+require 'pry'; binding.pry
 end
