416b31d7a9b5600a57959dbfb3badf7223280624
Jade Stewart <jadekstewart3@gmail.com>
2022-11-30 09:43:10 -0700

    Feat: add closing_time method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,4 +8,12 @@ class Restaurant
         @opening_time = opening_time
         @dishes = []
     end
+
+    def closing_time(hours)
+        "#{@opening_time.to_i + hours}:00"
+    end
+
+    # def add_dish(dish)
+    #     @dishes << dish
+    # end
 end
