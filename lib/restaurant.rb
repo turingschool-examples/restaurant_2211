abf087bd7f1d0818147934bb238c8be9389c306f
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 10:24:26 -0700

    complete iteration 2.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -22,9 +22,9 @@ class Restaurant
         (@opening_time.to_i + hours).to_s + ':00'
     end
 
-    #def add_dish(dish)
-     #   @dishes << dish
-    #end
+    def add_dish(dish)
+        @dishes << dish
+    end
 
    #def open_for_lunch?
     #    if @opening_time <= "12:00"
