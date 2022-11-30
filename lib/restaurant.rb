f0871d1958509af3e1f0037973ae26f46961e6e0
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-11-30 10:05:38 -0700

    Add test 3

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,16 @@
 class Restaurant
-    attr_reader :opening_time, :name, :dishes
+    attr_reader :opening_time, :name, :dishes, :closing_time, :add_dish
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
   end
+
+  def closing_time(hours_open)
+   "#{opening_time.to_i + hours_open}:00"
+  end
+
+#   def add_dish(dish)
+#     @dishes << dish
+#   end
 end
