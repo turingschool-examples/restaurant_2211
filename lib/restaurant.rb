a9953cc3f6f210cb83953fdab95cdbee764e3775
Weston Sandfort <sandfortw@gmail.com>
2022-11-30 09:21:12 -0700

    Initialize restaurant.rb

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,18 @@
 class Restaurant
+attr_reader :name, :opening_time, :dishes, :closing_time
+
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(hours_after_opening_time)
+    @opening_time += hours_after_opening_time
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
   
 end
