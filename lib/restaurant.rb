543dddb18f9af1c85d2f2ee6042cf0f65fc4ad9b
Brad Dunlap <braddunlapdpt@gmail.com>
2022-11-30 11:37:23 -0500

    Add closing time

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,6 +5,14 @@ class Restaurant
     @opening_time = opening_time
     @name = name
     @dishes = []
+    
+  end
+
+  def closing_time(hours_of_op)
+    open_int = opening_time.to_i
+    closed = open_int + hours_of_op
+    "#{closed}:00"
+    
   end
             
 end
