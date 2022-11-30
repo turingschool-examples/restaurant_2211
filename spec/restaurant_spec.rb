06966d0fce099ab2430e6d558c4f43b4328b2d7e
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-11-30 10:39:21 -0700

    Created tests for iteration 3

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -35,4 +35,24 @@ RSpec.describe Restaurant do
       expect(restaurant.dishes).to eq(['Burrata', 'Pizzetta', 'Ravioli'])
     end
   end
+
+  describe 'Iteration 3' do
+    it 'is open for lunch?' do
+      restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+      restaurant2 = Restaurant.new('16:00', 'Il Posto')
+
+      expect(restaurant1.open_for_lunch?).to eq(true)
+      expect(restaurant2.open_for_lunch?).to eq(false)
+    end
+  
+    it 'has menu dish names' do
+      restaurant2 = Restaurant.new('16:00', 'Il Posto')
+  
+      restaurant2.add_dish('Burrata')
+      restaurant2.add_dish('Pizzetta')
+      restaurant2.add_dish('Ravioli')
+
+      expect(restaurant2.menu_dish_names).to eq(["BURRATA", "PIZZETTA", "RAVIOLI"])
+    end
+  end
 end
