a78ebfaf96a4f2a3af845750f42ef3ec8882e58e
Carol Rieping <carol.rieping@gmail.com>
2022-11-30 10:44:00 -0700

    Add iteration 3 tests 1,2,3

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,9 +1,14 @@
 class Restaurant
-  attr_reader :opening_time, :name, :dishes
+  attr_reader :opening_time, :name, :dishes, :closing_time
 
     def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
     @dishes = []
+    @closing_time = closing_time
+    end
+
+    def add_dish(dishes)
+        @dishes<<dishes
     end
 end
