947d81cc8b431b6eeb6683c958a7a108f2b8dff7
Tori Enyart <torienyart@gmail.com>
2022-11-30 09:59:15 -0700

    Create closing_time method and test

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,10 +1,20 @@
 class Restaurant
     
-    attr_reader :opening_time, :name, :dishes
+    attr_reader :opening_time,
+     :name, 
+     :dishes, 
+     :hours_past_open,
+     :closing_time
     def initialize(opening_time, name)
         @opening_time = opening_time
         @name = name
         @dishes = []
+        @closing_time = ""
     end
   
+    def closing_time(hours_past_open)
+        closing_time_integer =
+        @opening_time[0..1].to_i + hours_past_open
+        @closing_time = "#{closing_time_integer}:00"
+    end
 end
