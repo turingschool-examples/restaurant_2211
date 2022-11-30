922de0b46eb6540a2d5f7ce51854b043c7454c6e
Andra Helton <andrahelton@gmail.com>
2022-11-30 09:31:24 -0700

    Add code for second test

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,9 @@
 class Restaurant
-    attr_reader :opening_time, :name
+    attr_reader :opening_time, :name, :dishes
     def initialize(opening_time, name)
         @opening_time = opening_time
         @name = name
+        @dishes = []
     
     end
   
