73e9dfd8fd3505cd9156eca0046cce0d6065231c
Elle Majors <hellolovely@ellemajors>
2022-11-30 09:45:10 -0700

    test: iteration 2

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Restaurant do
       expect(restaurant.name).to eq('Fuel Cafe')
     end
 
-    xit 'Starts with an empty array of dishes' do
+    it 'Starts with an empty array of dishes' do
       restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
       expect(restaurant.dishes).to eq([])
@@ -20,18 +20,22 @@ RSpec.describe Restaurant do
   end
 
   describe 'Iteration 2' do
-    xit 'returns closing time' do
+    it 'returns closing time' do
       restaurant = Restaurant.new('10:00', 'Fuel Cafe')
       expect(restaurant.closing_time(8)).to eq("18:00")
+   end
+    # it 'returns closing time' do
+    #   restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+    #   expect(restaurant.closing_time(8)).to eq("18:00")
 
-      @restaurant1.add_dish('Burrata')
-      @restaurant1.add_dish('Pizzetta')
-      @restaurant1.add_dish('Ravioli')
+    #   @restaurant1.add_dish('Burrata')
+    #   @restaurant1.add_dish('Pizzetta')
+    #   @restaurant1.add_dish('Ravioli')
 
-      expect(@restaurant1.dishes).to eq(['Burrata', 'Pizzetta', 'Ravioli'])
-    end
+    #   expect(@restaurant1.dishes).to eq(['Burrata', 'Pizzetta', 'Ravioli'])
+    # end
 
-    xit 'can add dishes' do
+    it 'can add dishes' do
       restaurant = Restaurant.new('10:00', 'Fuel Cafe')
       
       restaurant.add_dish('Burrata')
