44d8f55e4dd85e35802e88079cc2e8465ce0f39e
Weston Sandfort <sandfortw@gmail.com>
2022-11-30 10:07:18 -0700

    Add through iteration 4

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -26,8 +26,12 @@ attr_reader :name, :opening_time, :dishes, :closing_time
     return menu 
   end
 
-  def announce_closing_time
+  def announce_closing_time(hours_after_opening_time)
     closing_time = @opening_time.chop.chop.chop.to_i + hours_after_opening_time
+    am_or_pm = 0
+    closing_time >= 12 ? am_or_pm = "PM" : am_or_pm = "AM"
+    closing_time > 12 ? closing_time = closing_time - 12 : closing_time
 
+    return "#{@name} will be closing at #{closing_time}:00#{am_or_pm}"
   end
 end
