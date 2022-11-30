f0aa4d941b1049ab5f7a1922fc68d859fea72418
Elle Majors <hellolovely@ellemajors>
2022-11-30 10:25:45 -0700

    test: iteration 4

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -65,4 +65,14 @@ RSpec.describe Restaurant do
       expect(restaurant2.menu_dish_names).to eq(["BURRATA", "PIZZETTA", "RAVIOLI"])
     end
   end
+
+  # describe 'Iteration 4' do
+  #   it 'returns if it is open for lunch' do
+  #     restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
+  #     restaurant2 = Restaurant.new('16:00', 'Il Posto')
+      
+  #     expect(restaurant1.announce_closing_time(5)).to eq("Fuel Cafe will be closing at 11:00AM")
+  #     expect(restaurant2.announce_closing_time(7)).to eq("Il Posto will be closing at 11:00PM")
+  #   end
+  # end
 end
