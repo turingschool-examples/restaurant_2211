b827df5c9aa3b5863487bd145028ee8d9286beb5
Nickolas Marquez <nickomarquez7@gmail.com>
2022-11-30 12:03:54 -0700

    Complete 1 IC

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant
-  
+    attr_reader :restaurant,
+                :opening_time,
+                :name
+
+    def initialize(restaurant)
+        @restaurant = restaurant
+        @opening_time =[]
+        @name = []
+    end    
 end
