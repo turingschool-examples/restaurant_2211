df48eacb59fcfcff45f616222f9adee2881b583d
Bobby Luly <bobbyluly@gmail.com>
2022-11-30 10:52:36 -0700

    Create iteration 4 test

--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -54,9 +54,19 @@ RSpec.describe Restaurant do
       restaurant2.add_dish('Burrata')
       restaurant2.add_dish('Pizzetta')
       restaurant2.add_dish('Ravioli')
-      
+
       expect(restaurant2.menu_dish_names).to eq(["BURRATA", "PIZZETTA", "RAVIOLI"])
     end
-
   end
+
+  describe 'Iteration 4' do
+    it 'announces a closing time' do
+      restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
+      restaurant2 = Restaurant.new('16:00', 'Il Posto')
+
+      expect(restaurant1.announce_closing_time(5)).to eq("Fuel Cafe will be closing at 11:00AM")
+      expect(restaurant2.announce_closing_time(7)).to eq("Il Posto will be closing at 11:00PM")
+    end
+  end
+
 end
