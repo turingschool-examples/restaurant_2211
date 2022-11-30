f9e79adf00580c6dbb1e32ce290556f5f5ce7a5f
Hady Matar <hadyematar@gmail.com>
2022-11-30 09:46:22 -0700

    Complete first test of iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -3,9 +3,26 @@ class Restaurant
     attr_reader :opening_time,
                 :name, 
                 :dishes
+                :name_of_dish
+
     def initialize(opening_time, name)
         @opening_time = opening_time
         @name = name  
         @dishes = []
+    end 
+
+    def closing_time(time)
+        integer = opening_time.delete(":")
+        time_as_s = integer.chomp('00')
+        time_as_i = time_as_s.to_i
+        closing_time = time_as_i+time 
+        closing_time_s = closing_time.to_s
+        return closing_time_s.concat(":00")
+
+    end 
+
+    def add_dish(name_of_dish)
+        @name_of_dish = name_of_dish
+        @dishes << name_of_dish
     end 
 end
--- spec/restaurant_spec.rb
+++ spec/restaurant_spec.rb
@@ -4,35 +4,30 @@ require './lib/restaurant'
 
 
 RSpec.describe Restaurant do
-  describe 'Iteration 1' do
-    it 'Exists and has attributes' do
-      restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+  # describe 'Iteration 1' do
+  #   it 'Exists and has attributes' do
+  #     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
-      expect(restaurant).to be_a(Restaurant)
-      expect(restaurant.opening_time).to eq('10:00')
-      expect(restaurant.name).to eq('Fuel Cafe')
-    end
+  #     expect(restaurant).to be_a(Restaurant)
+  #     expect(restaurant.opening_time).to eq('10:00')
+  #     expect(restaurant.name).to eq('Fuel Cafe')
+  #   end
 
-    it 'Starts with an empty array of dishes' do
-      restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+  #   it 'Starts with an empty array of dishes' do
+  #     restaurant = Restaurant.new('10:00', 'Fuel Cafe')
 
-      expect(restaurant.dishes).to eq([])
-    end
-  end
+  #     expect(restaurant.dishes).to eq([])
+  #   end
+  # end
 
   describe 'Iteration 2' do
-    xit 'returns closing time' do
+    it 'returns closing time' do
       restaurant = Restaurant.new('10:00', 'Fuel Cafe')
       expect(restaurant.closing_time(8)).to eq("18:00")
 
-      @restaurant1.add_dish('Burrata')
-      @restaurant1.add_dish('Pizzetta')
-      @restaurant1.add_dish('Ravioli')
-
-      expect(@restaurant1.dishes).to eq(['Burrata', 'Pizzetta', 'Ravioli'])
     end
 
-    xit 'can add dishes' do
+    it 'can add dishes' do
       restaurant = Restaurant.new('10:00', 'Fuel Cafe')
       
       restaurant.add_dish('Burrata')
