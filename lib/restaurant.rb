e834ee75eca543f9449d77ebd048c0c9221a90a7
Elle Majors <hellolovely@ellemajors>
2022-11-30 09:11:09 -0700

    Initial commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant
-  
+  attr_reader :opening_time,
+              :name,
+              :dishes
+
+  def initialize(opening_time,name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
 end
