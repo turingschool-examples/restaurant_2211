81e16d278c41e2f95cc17ae8fceec7df0bf0b2c7
Bobby Luly <bobbyluly@gmail.com>
2022-11-30 09:42:23 -0700

    Add add_dish method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,5 +6,13 @@ class Restaurant
         @name = name
         @dishes = []
     end
+
+    def closing_time(time)
+        "#{@opening_time.to_i + time}:00"
+    end
+
+    def add_dish(dish)
+        @dishes << dish
+    end
   
 end
