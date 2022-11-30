6863495f98a61db0ded63a1dcdb50c012568ffb7
Axel De La Guardia <axeldelaguardia@gmail.com>
2022-11-30 09:12:55 -0700

    Pass closing time test and add closing_time method.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,15 @@
 class Restaurant
-  attr_reader :name, :opening_time
+  attr_reader :name, :opening_time, :dishes
 
   def initialize(opening_time, name)
     @opening_time = opening_time
     @name = name
+    @dishes = []
   end
+
+  def closing_time(time)
+    "#{@opening_time.to_i + time}:00"
+  end
+
+
 end
