5b8a27dc91e9a8c3c2a1960520ad78e418c0caf3
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 09:27:32 -0700

    initial commit.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,18 @@
 class Restaurant
   
+    def initialize(opening_time, name)
+        @opening_time = opening_time
+        @name = name
+
+    end
+
+    def opening_time
+        @opening_time
+    end
+
+    def name
+        @name
+    end
+
+
 end
