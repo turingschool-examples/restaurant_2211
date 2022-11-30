6773580307607181ce841e7a9042184c35299ee9
Caleb Thomas <cjthomas00@comcast.net>
2022-11-30 10:22:55 -0700

    complete closing time prroblem.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -17,6 +17,20 @@ class Restaurant
     def dishes
         @dishes 
     end
+    
+    def closing_time(hours)
+        (@opening_time.to_i + hours).to_s + ':00'
+    end
 
+    #def add_dish(dish)
+     #   @dishes << dish
+    #end
+
+   #def open_for_lunch?
+    #    if @opening_time <= "12:00"
+     #       true
+      #  else
+       # end
+    #end
 
 end
