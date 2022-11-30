7fb32616d3797bcd9340d32208ed8c7a74c333ab
Elle Majors <hellolovely@ellemajors>
2022-11-30 09:46:29 -0700

    feat: #closing_time and #add_dish

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -8,4 +8,12 @@ class Restaurant
     @name = name
     @dishes = []
   end
+
+  def closing_time(hours)
+    (@opening_time.to_i + hours).to_s + ':00'
+  end
+
+  def add_dish(dish)
+    @dishes << dish
+  end
 end
