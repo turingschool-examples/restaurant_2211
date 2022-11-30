bd2df06ae573dc6af440ec2b03a46b40182e4e4e
Jade Stewart <jadekstewart3@gmail.com>
2022-11-30 09:30:20 -0700

    Initial commit

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant
-  
+    attr_reader :opening_time,
+                :name
+    
+    def initialize(opening_time, name)
+        @name = name
+        @opening_time = opening_time
+    end
 end
