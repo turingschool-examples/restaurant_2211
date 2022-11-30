fa51e7b993bb8bf652c3666f07465c13aa2c498c
Chris Crane <chrisjcrane@gmail.com>
2022-11-30 09:38:20 -0700

    pass iteration 1 test 1

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant
-  
+  attr_reader :opening_time, :name, :dishes
+
+  def initialize(opening_time, name, dishes = [])
+    @opening_time = opening_time
+    @name = name
+    @dishes = dishes
+  end
 end
