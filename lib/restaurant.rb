696f392405afba00f846a9c5e6df64f90464e53f
David Marino <davidjmarino8@gmail.com>
2022-11-30 12:36:19 -0500

    Itteration 2 complete

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -5,7 +5,16 @@ class Restaurant
     def initialize(opening_time, name)
         @opening_time = opening_time
         @name = name
+        @dishes = []
         
+    end
+
+    def closing_time(number)
+        "#{opening_time.to_i + number}:00"
+    end
+
+    def add_dish(dish)
+        @dishes << dish
     end
 
   
