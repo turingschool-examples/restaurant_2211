139669e829d7b6798ecf204b7969b00e24a60dcc
Jasmine Hamou <jasmine.mahabadi@gmail.com>
2022-11-30 08:23:01 -0800

    Pass 'starts with an empty array of dishes' test

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,8 +1,10 @@
 class Restaurant
     attr_reader :opening_time,
-                :name
+                :name,
+                :dishes
   def initialize (opening_time, name)
     @opening_time = opening_time
     @name = name
+    @dishes = []
   end 
 end
