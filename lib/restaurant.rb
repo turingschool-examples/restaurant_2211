221f24edf3a7a52fcfbeaaa62be388f6dc36d7e9
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2022-11-30 10:42:40 -0700

    Created tests for iteration 3

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -18,6 +18,13 @@ class Restaurant
   
   def add_dish(dish)
     @dishes << dish
+  end
 
+  def open_for_lunch?(opening_time)
+    if opening_time.chomp(':00').to_i < 12
+      true
+    else
+      false
+    end
   end
 end
