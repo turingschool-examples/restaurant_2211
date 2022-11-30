b39b677516d58f3d3c77d64470fd0977d90ac458
Adam Bailey <ab67319@gmail.com>
2022-11-30 10:09:06 -0700

    Unskip Iteration 2 returns closing time test and create closing_time method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -2,10 +2,16 @@ class Restaurant
     attr_reader :dishes,
                 :name,
                 :opening_time
-                
+
   def initialize(opening_time, name)
     @dishes = []
     @name = name
     @opening_time = opening_time
   end
+
+  def closing_time(hours)
+    closing_time = @opening_time.chars[0, @opening_time.index(":")].join.to_i + hours
+    return "#{closing_time}:00"
+  end
+
 end
