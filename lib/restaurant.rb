aa11878ca6d39564cecaa2c327314fbe9704c946
Isaac Alter <i_alter@yahoo.com>
2022-11-30 09:49:13 -0700

    Add the finished second iteration

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,11 +1,22 @@
 class Restaurant
     attr_reader :opening_time,
                 :name,
-                :dishes
+                :dishes,
+                :restaurant
 
     def initialize(opening_time, name)
         @opening_time = opening_time
         @name = name
         @dishes = []
+    end
+
+    def closing_time(time)
+        open = opening_time.delete_suffix(":00").to_i
+        close = open + time
+        return "#{close}:00"
+    end
+
+    def add_dish(dish)
+        dishes << dish
     end
 end
