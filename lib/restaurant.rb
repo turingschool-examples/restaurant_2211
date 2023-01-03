09f3a5efdc9b3f5653bf6d0fd181c2f31722dde1
Nickolas Marquez <nickomarquez7@gmail.com>
2023-01-03 10:59:49 -0700

    Wrong ic

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -3,9 +3,9 @@ class Restaurant
                 :opening_time,
                 :name
 
-    def initialize(restaurant)
-        @restaurant = restaurant
-        @opening_time =[]
-        @name = []
+    def initialize(info)
+        # @restaurant = restaurant
+        @opening_time =info[:opening_time]
+        @name = info[:name]
     end    
 end
